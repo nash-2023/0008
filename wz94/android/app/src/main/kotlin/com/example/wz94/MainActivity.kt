842feba1384c0f7a491abe85ad0cb6172ad2fd53
nash-2023 <abdalla.fat7y@hotmail.com>
2022-10-24 09:24:29 +0200

    wz 94 flutter native splash

---
+++ wz94/android/app/src/main/kotlin/com/example/wz94/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wz94
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wz94/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
+  runApp(const MyApp());
+}
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("the home page"),
+      ),
+    );
+  }
+}
+
+/**
+ * flutter pub add flutter_native_splash
+ * 
+ * -- create a brand new file called (flutter_native_splash.yaml)
+ * and past in it the configuration from the site 
+ * 
+ * flutter pub run flutter_native_splash:create
+ */
