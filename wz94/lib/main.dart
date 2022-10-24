2df3dbd51444e32d7b02037661d7f5b523fa0648
nash-2023 <abdalla.fat7y@hotmail.com>
2022-10-24 16:35:33 +0200

    wz 97

--- wz94/lib/main.dart
+++ wz94/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -38,11 +39,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future getPosition() async {
+    late bool services;
+    services = await Geolocator.isLocationServiceEnabled();
+    print("location service enabled $services");
+    if (services) {
+      AwesomeDialog(
+          context: context,
+          title: "Location Services",
+          body: Text("Location Services Not Enabled"))
+        ..show();
+    }
+  }
+
+  void initState() {
+    getPosition();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("the home page"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [],
       ),
     );
   }
@@ -56,3 +77,23 @@ class _MyHomePageState extends State<MyHomePage> {
  * 
  * flutter pub run flutter_native_splash:create
  */
+
+/*
+flutter pub add geolocator
+
+// var a;
+  // Future getData() async {
+  //   print("Before");
+  //   await Future.delayed(Duration(seconds: 5), () {
+  //     print("delay");
+  //     a = 10;
+  //   });
+  //   print("After");
+  //   print(a);
+  // }
+
+  // void initState() {
+  //   getData();
+  //   super.initState();
+  // }
+*/
